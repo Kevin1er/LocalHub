df6b770aae42ce765de4b50a130807dc6d0f03a1
Kevin1er <vocanson.kevin1@gmail.com>
2019-01-23 15:06:54 +0100

    First commit

---
+++ js/app.js
@@ -0,0 +1,78 @@
+search_focus = false;
+lang = "fr";
+lang_focus = false;
+fr_search = "Entrez une question ...";
+en_search = "Enter a question ...";
+
+function searchFocus() {
+    document.getElementById("form").style.borderColor = "rgba(81, 203, 238, 1)";
+    document.getElementById("form").style.boxShadow = "0 0 5px rgba(81, 203, 238, 1)";
+    search_focus = true;
+}
+
+function searchBlur() {
+    document.getElementById("form").style.borderColor = "gray";
+    document.getElementById("form").style.boxShadow = "none";
+    search_focus = false;
+}
+
+function searchOver() {
+    if(!search_focus) document.getElementById("form").style.borderColor = "#333333";
+}
+
+function searchOut() {
+    if(!search_focus) document.getElementById("form").style.borderColor = "gray";
+}
+
+/*
+function langOver() {
+    if(lang == "fr" && !lang_focus)
+    {
+        document.getElementById("lang-icon").src="../ressources/uk.png";
+        lang = "en";
+        lang_focus = true;
+    }
+    else if(lang == "en" && !lang_focus)
+    {
+        document.getElementById("lang-icon").src="../ressources/fr.png";
+        lang = "fr";
+        lang_focus = true;
+    }
+}
+
+function langOut() {
+    lang_focus = false;
+}
+*/
+
+/*
+* OnClick event on langage button
+*/
+function langClick() {
+    if(lang == "fr")
+    {
+        document.getElementById("lang-icon").src="../ressources/uk.png";
+        langChange();
+    }
+    else if(lang == "en")
+    {
+        document.getElementById("lang-icon").src="../ressources/fr.png";
+        langChange();
+    }
+}
+
+/*
+* Change langage function
+*/
+function langChange() {
+    if(lang == "fr")
+    {
+        document.getElementById("search").placeholder = en_search;
+        lang = "en";
+    }
+    else if(lang == "en")
+    {
+        document.getElementById("search").placeholder = fr_search;
+        lang = "fr";
+    }
+}
