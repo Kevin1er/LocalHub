4d3945b8cd80c4caf1f87fe8d86c947f73602016
Kevin1er <vocanson.kevin1@gmail.com>
2019-02-12 10:34:01 +0100

    import_v1.1

--- js/fileImport.js
+++ js/fileImport.js
@@ -1,9 +1,23 @@
 // read a CSV file
 var csvParser = new SimpleExcel.Parser.CSV();
 var fileInput = document.getElementById('fileInput');
-// parse when file loaded, then print the result to console
+var dropzoneInput = document.getElementById('dropZoneInput');
+
+
+/**
+ * Event listener fileInput
+ */
 fileInput.addEventListener('change', function (e) 
 {  
+    var file = e.target.files[0];
+    loadFile(file);
+});
+
+/**
+ * Event listener dropZoneInput
+ */
+dropzoneInput.addEventListener('drop', function(e)
+{
     var file = e.target.files[0];
     loadFile(file);
 });
@@ -15,5 +29,23 @@ function loadFile(file)
 {
     csvParser.loadFile(file, function () {
         console.log(csvParser.getSheet()); // print!
+    });
+}
+
+
+/**
+ * ScanTest Function
+ */
+function scanTest()
+{
+    // const html = fetch("https://www.ville-lechambonsurlignon.fr/professionnels-sante/medecins-1.html").text(); // html as text
+    // const doc = new DOMParser().parseFromString(html, 'text/html');
+    // doc.title; doc.body;
+    // console.log(doc.title);
+
+    var name = "medecins-1";
+    var url = "https://www.ville-lechambonsurlignon.fr/professionnels-sante/medecins-1.html";
+    $.get(url, function(response) {
+        console.log(response);
     });
 }
